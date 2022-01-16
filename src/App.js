7f3107055f6e2d917f277005f56da362d03675c1
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-01-16 22:59:26 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import Header from './Header'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div>
        <div className="app">
-          {/* Header */}
           <Header/>
-          {/* Home */}
+          <Home />
 
         </div>
     </div>
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return <div className="home">
+      <div className="home__container">
+        img
+      </div>
+  </div>;
+}
+
+export default Home;
