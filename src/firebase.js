fa6b4eed2f20ae0147d4fee8ca51b687d6e1c912
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-01-16 22:06:01 +0700

    1

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCLtgestLygS9Xn1cN4e4NiqjxzS3R9Zq4",
+  authDomain: "clone-4e2cd.firebaseapp.com",
+  projectId: "clone-4e2cd",
+  storageBucket: "clone-4e2cd.appspot.com",
+  messagingSenderId: "728149221245",
+  appId: "1:728149221245:web:3eb58671e307993fe4f614",
+  measurementId: "G-C8T2N806GV",
+};
