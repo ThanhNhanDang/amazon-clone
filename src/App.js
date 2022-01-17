8cbdaddb79f674337a3f6ef753a33956ca91d78c
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-01-17 15:01:14 +0700

    home page and checkout page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Home from './components/home/Home'
-
+import React from "react";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./components/checkout/Checkout";
 function App() {
   return (
-    <div>
-       <div className="app">
-          <Header/>
-          <Home />
-
-        </div>
-    </div>
-  )
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/checkout" exact element={<Checkout />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
